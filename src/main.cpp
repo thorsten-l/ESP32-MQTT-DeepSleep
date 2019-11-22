f3facb3272ce47c9f8928768497ed83ded9a7867
Thorsten Ludewig <th@l9g.de>
2019-11-22 13:20:19 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,82 @@
+#include <App.hpp>
+#include <Arduino.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
+
+RTC_DATA_ATTR int bootCounter = 0; // This var ist stored in ulp/rtc data
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+time_t startTime = millis();
+
+void setup()
+{
+  Serial.begin(115200);
+
+  Serial.println("WiFi config.");
+
+  WiFi.mode(WIFI_OFF);
+  WiFi.disconnect(true);
+
+  WiFi.config(
+    IPAddress(IP_HOST), 
+    IPAddress(IP_GATEWAY), 
+    IPAddress(IP_NETMASK),
+    IPAddress(IP_DNS1)
+  );
+
+  Serial.println("WiFi begin.");
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(250);
+    if ((millis() - startTime) > CONNECTION_TIMEOUT)
+    {
+      Serial.println("\n\n*** WiFi connection timeout ***\n\n");
+      ESP.restart();
+    }
+  }
+
+  Serial.println("\nWiFi connected.");
+
+  client.setServer(MQTT_HOST, MQTT_PORT);
+  Serial.println("Connecting to MQTT server.");
+}
+
+void loop()
+{
+
+#ifdef MQTT_USEAUTH
+    client.connect(MQTT_CLIENTID, MQTT_USER, MQTT_PASSWORD);
+#else
+    client.connect(MQTT_CLIENTID);
+#endif
+
+  if (client.connected())
+  {
+    Serial.println("MQTT server connected.");
+
+    char buffer[32];
+    time_t endTime = millis();
+    sprintf(buffer, "%d %lums", bootCounter++, endTime - startTime);
+
+    client.publish(MQTT_OUTTOPIC, buffer, true);
+    delay(100);
+    Serial.printf("%s : value send.\n", buffer);
+
+    ESP.deepSleep(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  }
+  else
+  {
+    if ((millis() - startTime) > CONNECTION_TIMEOUT)
+    {
+      Serial.println("\n\n*** MQTT connection timeout ***\n\n");
+      ESP.restart();
+    }
+  }
+
+  delay(500);
+}
